18d81a885fca6e205b33f12181e042f31ac01d79
Gerald Allay <geraldallay@gmail.com>
2020-08-21 14:11:34 +0300

    Feat: Add endpoint to delete timestamps Delete timestamps on heroku app

--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,18 @@
 import regionRoutes from './region/region.routes.js';
+import Region from '../modules/region/region.model.js';
 
 export default app => {
 
   app.use('/api/regions', regionRoutes);
+
+  app.get('/delete-timestamps', async (req, res) => {
+    const response = await Region.collection.update({},
+        {$unset: {createdAt: true, updatedAt: true}},
+        {multi: true, safe: true}
+    );
+
+    res.json(response)
+  });
 
   app.use('*', (req, res) => {
     return res.status(404).json({status: false, message: 'Resource Not found'});
