78ce95527b64e8d07c666d85c0a1599cc8073d71
Gerald Allay <geraldallay@gmail.com>
2020-08-21 14:14:39 +0300

    Feat: Remove endpoint to delete timestamps

--- src/modules/index.js
+++ src/modules/index.js
@@ -5,15 +5,6 @@ export default app => {
 
   app.use('/api/regions', regionRoutes);
 
-  app.get('/delete-timestamps', async (req, res) => {
-    const response = await Region.collection.update({},
-        {$unset: {createdAt: true, updatedAt: true}},
-        {multi: true, safe: true}
-    );
-
-    res.json(response)
-  });
-
   app.use('*', (req, res) => {
     return res.status(404).json({status: false, message: 'Resource Not found'});
   });
